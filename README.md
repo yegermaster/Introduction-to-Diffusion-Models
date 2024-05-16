e1477a55f94f4b08c238767d38df63edccc10419
Owner <itamaryeger3@gmail.com>
2024-05-16 15:01:13 +0300

    added forward_process explanation and impelmantation

--- README.md
+++ README.md
@@ -8,6 +8,27 @@ This paper introduces the application of Latent Diffusion Models (LDM) in recons
 
 ![](img/dall-e2.png)
 
+## Stable diffuision
+Think of stable diffusion like a sophisticated game of telephone, but with math. The model uses a stochastic process (fancy term for randomness), where each state depends only on the previous one, forming what's known as a Markov chain. This iterative method transitions from one distribution to another, 
+
+
+
+The model employs a stochastic (fancy term for randomness), where each state depends only on the previous state. The method iteratively transitions from one distribution to another, an idea used in non-equilibrium thermodynamics, and so it's effectively tracing and reconstructing lost information.
+much like how your coffee diffuses into water—starting with a Gaussian distribution and transforming it into a target distribution.
+
+![alt text](410c9afff36949d7903e186d8cc46209.webp)
+
+So, just like our ink in water, we first immerse the data into noise (the forward process or encoder) and then trace it back to the original information (the backward process or decoder).
+
+## Forward process
+Check out the `forward_process.py` file for an example of the code in action.
+
+
+![alt text](image.png)
+
+## Backward Process
+
 ## Implementation of U-Net in LDM
 
 The U-Net architecture plays a crucial role in the functionality of Latent Diffusion Models. Originally developed for biomedical image segmentation, U-Net has been adapted for the generative tasks of LDMs to enhance the quality and precision of the image generation process. Here, we provide TensorFlow code snippets to illustrate the implementation of U-Net within the LDM framework, emphasizing how this integration aids in detailed and accurate image reconstruction
+
---
+++ code/forward_process.py
@@ -0,0 +1,78 @@
+"""
+Diffusion Models: Visual Information Reconstruction in Neural Networks
+
+This module introduces the Forward procress of Diffusion Models (DM). 
+focusing particularly on the use of the U-Net architecture within these models.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def image_normalize(size:tuple, image_path:str)->np.array:
+    """
+    Normalize an image by resizing and scaling pixel values to the [0, 1] range.
+
+    Parameters:
+    size (tuple): The target size for the image.
+    image_path (str): The path to the image.
+
+    Returns:
+    np.array: The normalized image as a NumPy array.
+    """
+    # Load the input image
+    image = Image.open(image_path).convert('L')  # Convert to grayscale
+    image = image.resize(size)
+    return np.array(image) / 255.0  # Normalize to [0, 1] 
+
+
+def forward_diffusion_process(image: np.array, steps: int, scheduler: np.array) -> list:
+    """
+    Perform the forward diffusion process on an image.
+
+    Parameters:
+    x (np.array): The input image as a normalized NumPy array.
+    T (int): The number of diffusion steps.
+    scheduler (np.array): The diffusion scheduler array.
+
+    Returns:
+    list: A list of images at each diffusion step.
+    """
+    image_list = [image]
+
+    for step in range(1, steps + 1):
+        epsilon = np.random.normal(0, 1, image.shape)
+        noised_image = np.sqrt(1 - scheduler[step - 1]) * image_list[-1] + np.sqrt(scheduler[step - 1]) * epsilon
+        image_list.append(noised_image)
+    
+    return image_list
+
+def plot_images(steps: int, image_list: list) -> plt:
+    """
+    Plot images at each step of the diffusion process.
+
+    Parameters:
+    T (int): The number of diffusion steps.
+    x_list (list): The list of images at each diffusion step.
+
+    Returns:
+    plt: The plot object with the displayed images.
+    """
+    _, axes = plt.subplots(1, steps + 1, figsize=(15, 5))
+    for step, noised_image in enumerate(image_list):
+        axes[step].imshow(noised_image, cmap='gray')
+        axes[step].set_title(f"Step {step}", fontsize=10)
+        axes[step].axis('off')
+    plt.show()
+
+if __name__ == "__main__":
+    T = 10
+    beta = np.linspace(0.01, 0.2, T)
+    # Normalize the input image
+    x = image_normalize((512, 512), 'img/elephant.jpeg')
+
+    # Run forward diffusion process
+    x_list = forward_diffusion_process(x, T, beta)
+
+    # Plot the results
+    plot_images(T, x_list)
